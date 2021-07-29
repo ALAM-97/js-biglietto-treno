065894e0902dc4c0314adb695394ffb572aead55
ALAM-97 <alexamara97@gmail.com>
2021-07-29 17:38:24 +0200

    Style Added to the Page and Script Refactured

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-// 1. chiedere num di KM da percorrere
-var kmViaggio = parseInt(prompt("Digita il numero di KM da percorrere"));
-// 2. chiedere età del passeggero
-var etaPax = parseInt(prompt("Digita la tua età"));
-// 3. calcolare costo del biglietto
-var costoBiglietto = kmViaggio * 0.21;
-// 4. applicare lo sconto in base all'età del passeggero
-if (etaPax < 18) {
-    costoBiglietto = (kmViaggio * 0.21) * 0.20
-} else if (etaPax > 65) {
-    costoBiglietto = (kmViaggio * 0.21) * 0.40
+function calcoloCostoBiglietto() {
+    // 1. chiedere num di KM da percorrere
+    var kmViaggio = parseInt(prompt("Digita il numero di KM da percorrere"));
+    // 2. chiedere età del passeggero
+    var etaPax = parseInt(prompt("Digita la tua età"));
+    // 3. calcolare costo del biglietto
+    var costoBiglietto = kmViaggio * 0.21;
+    // 4. applicare lo sconto in base all'età del passeggero
+    if (etaPax < 18) {
+        costoBiglietto = (kmViaggio * 0.21) * 0.20
+    } else if (etaPax > 65) {
+        costoBiglietto = (kmViaggio * 0.21) * 0.40
+    }
+    // 5. imposto solo 2 cifre dopo la virgola
+    costoBiglietto = costoBiglietto.toFixed(2)
+    // 6. mostra costo del biglietto nell'HTML
+    document.getElementById("your-price").innerHTML = "€ " + costoBiglietto;
 }
-// 5. imposto solo 2 cifre dopo la virgola
-costoBiglietto = costoBiglietto.toFixed(2)
